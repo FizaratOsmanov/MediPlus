3b31ff754c213785a7646fac1764e78d34a3f6c0
FizaretOsmanov <fizaretgithub@gmail.com>
2024-12-03 03:28:06 +0400

    .

--- MediPlus/MediPlus.BL/obj/Debug/net8.0/MediPlus.BL.AssemblyInfo.cs
+++ MediPlus/MediPlus.BL/obj/Debug/net8.0/MediPlus.BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediPlus.BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0c7987abb682cd277e71d35c82361eb80dd38e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6a483f466b3ca5b23ac0b46ca1891475b242c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediPlus.BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediPlus.BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
